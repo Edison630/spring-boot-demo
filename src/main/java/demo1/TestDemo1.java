ea1a5ff53742e8ef0bd19e1bf33ca7311b2876e2
Edison630 <73467868+Edison630@users.noreply.github.com>
2020-10-28 15:32:07 +0800

    Update TestDemo1.java

--- src/main/java/demo1/TestDemo1.java
+++ src/main/java/demo1/TestDemo1.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class TestDemo1 {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println();
+        
         System.out.println();
         System.out.println();
     }
