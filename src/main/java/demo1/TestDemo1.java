cc6996863f2c49c0d0de8df20cf6538dd7d4e1f8
Edison630 <73467868+Edison630@users.noreply.github.com>
2020-10-28 15:42:50 +0800

    Update TestDemo1.java

--- src/main/java/demo1/TestDemo1.java
+++ src/main/java/demo1/TestDemo1.java
@@ -4,8 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class TestDemo1 {
     public static void main(String[] args) {
-        System.out.println("1111111111111111");
-        System.out.println("888888888888888888888");
+
     }
 
 }
