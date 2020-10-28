28a7826d0af48e2b58eedd7419f2857c7f593992
sunjia <646508387@qq.com>
2020-10-28 15:36:23 +0800

    自定义starter

--- src/main/java/demo1/TestDemo1.java
+++ src/main/java/demo1/TestDemo1.java
@@ -4,14 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class TestDemo1 {
     public static void main(String[] args) {
-        System.out.println();
-        
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("1111111111111111");
     }
 
 }
