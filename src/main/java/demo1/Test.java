f31b3e889d0384c716e1ec6e5254d1b8478fbd9a
sunjia <646508387@qq.com>
2020-10-28 13:34:25 +0800

    自定义starter

--- src/main/java/demo1/Test.java
+++ src/main/java/demo1/Test.java
@@ -3,9 +3,22 @@ package demo1;
 import ncl.test.Myconfigration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.math.BigDecimal;
+
 public class Test {
     public static void main(String[] args) {
+
         AnnotationConfigApplicationContext act = new AnnotationConfigApplicationContext(Myconfigration.class);
         System.out.println(act.getBean("testRedis"));
+
+        int a = 1;
+        double b = 2;
+        BigDecimal aa = new BigDecimal(a);
+        BigDecimal bb = new BigDecimal(b);
+        aa.add(bb);//a+b
+        aa.subtract(bb);//a-b
+        aa.multiply(bb);//a乘以b
+        aa.divide(bb);//a除以b
     }
 }
